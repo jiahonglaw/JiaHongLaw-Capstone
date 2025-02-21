4f9d6d21e90018eb026bd0bbf99ad470063a8db0
jiahonglaw <law.jia.hong@gmail.com>
2025-02-22 04:31:40 +0800

    First edits

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,16 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <h1>Finance Dashboard</h1>
-      <div>
+      <h1>Finance</h1>
+      <div className="card">
         <button>
+          Add Stock
         </button>
         <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
