3b94ec58da7b635eb0e458d25eb809cda11a5511
jiahonglaw <law.jia.hong@gmail.com>
2025-02-22 04:25:32 +0800

    Installed Vite and React

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Finance Dashboard</h1>
+      <div>
+        <button>
+        </button>
+        <p>
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
