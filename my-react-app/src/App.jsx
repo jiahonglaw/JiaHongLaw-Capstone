f2e1b3900b8c5efbe8345b7f8b0a63bb71fb52a6
jiahonglaw <law.jia.hong@gmail.com>
2025-02-23 08:08:32 +0800

    Final edits

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,18 +7,16 @@ function App() {
 
     
     <div className="dashboard-container">
-      {/* Finance icon */}
-      <img src="budget-DTOxYgwc.png" alt="Finance Icon" className="finance-icon" />
-    
-      
       <h1>Finance Dashboard</h1>
 
         <form className="stock-form">
         <input type="text" placeholder="Stock Symbol"/><br /><br />
         <input type="number" placeholder="Quantity" /><br /><br />
         <input type="number" placeholder="Purchase Price" /><br /><br />
-        <input type="submit" value="Add Stock" />
+        <input className="add-stock-btn" type="submit" value="Add Stock" />
         </form>
+
+        
         
         {/* Stock List */}
         <h2>Stock List</h2>
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './Header.css'
+
+function Header() {
+
+  return (
+
+    
+    <div>
+      {/* Finance icon */}
+      <img src="budget-DTOxYgwc.png" alt="Finance Icon" className="img-container" />
+      
+        </div>
+  )
+}
+
+export default Header
