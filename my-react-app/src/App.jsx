8bdd45dbf276df02964c4cbcf3ab242fea9bb57a
jiahonglaw <law.jia.hong@gmail.com>
2025-03-01 22:32:42 +0800

    Update App button

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [stockSymbol, setStockSymbol] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [purchasePrice, setPurchasePrice] = useState("");
+ 
   return (
 
     
@@ -10,23 +13,43 @@ function App() {
       <h1>Finance Dashboard</h1>
 
         <form className="stock-form">
-        <input type="text" placeholder="Stock Symbol"/><br /><br />
-        <input type="number" placeholder="Quantity" /><br /><br />
-        <input type="number" placeholder="Purchase Price" /><br /><br />
-        <input className="add-stock-btn" type="submit" value="Add Stock" />
-        </form>
-
-        
+        <input type="text" 
+        placeholder="Stock Symbol" 
+        value={stockSymbol} 
+        onClick={(event)=> setStockSymbol(event.target.value)}/>
+        <br /><br />
+        <input type="number" 
+        placeholder="Quantity"
+        value={quantity}
+        onClick={(event)=> setQuantity(event.target.value)} />
+        <br /><br />
+        <input type="number" 
+        placeholder="Purchase Price" 
+        value={purchasePrice}
+        onClick={(event)=>setPurchasePrice(event.target.value)}
+        /><br /><br />
+        <button className="add-stock-btn" type="submit">Add Stock</button>
+        </form>        
         
         {/* Stock List */}
         <h2>Stock List</h2>
-        <p>No stocks added yet</p>
-        <p className="symbol">Symbol: AAPL</p>
-        <p>Quantity: 2</p>
-        <p>Purchase Price: 500</p>
-        <p>Current Price: 100</p>
-        <p className="gains"> Profit/Loss: +/- xxx.xx</p>
+        {stockSymbol.length ===0 ?(
+          <p>No stocks added yet</p>
+        ): (
+          stockSymbol.localeCompare((stock,index) =>(
+            <div key={index}>
+              <p className="symbol">Symbol:{stock.stockSymbol}</p>
+              <p>Quantity: {stock.quantity}</p>
+              <p>Purchase Price: {stock.purchasePrice} </p>
+              <p>Current Price: 100</p>
+              <p className="gains"> Profit/Loss: +/- xxx.xx</p>
+              </div>
+          ))
 
+
+        )}
+        
+        
 
         </div>
   )
